7b555e32fc45f2d338b4a5c947bd1455a3ecb20d
vvvxxq <vvvxxq@users.noreply.github.com>
2025-09-22 22:28:59 +0200

    fix: update ngx-spinner styles and component initialization

--- FreshCart/src/app/app.component.ts
+++ FreshCart/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  
+  constructor(private spinner: NgxSpinnerService) {}
+
+  ngOnInit() {
+    /** spinner starts on init */
+    this.spinner.show();
+  }
 }
